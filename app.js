2c795b90b6aec70fc0b6a8d9ce56bdec144db9a5
Santiago Lopez <santiago.lopez@i2cat.net>
2020-01-17 10:42:11 +0100

    wired button

---
+++ app.js
@@ -0,0 +1,6 @@
+const reason = document.querySelector('#reason');
+const amount = document.querySelector('#amount');
+const cancelBtn = document.querySelector('#btn-cancel');
+const confirmBtn = document.querySelector('#btn-confirm');
+
+confirmBtn.addEventListener('click', () => console.log('wired!'));
