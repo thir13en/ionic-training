7b34dbed078c3325bb6d04ae896c2f58c997eefa
santiloopz <santilpzate@gmail.com>
2020-01-18 17:52:37 +0100

    added clear button

--- app.js
+++ app.js
@@ -4,9 +4,16 @@ const cancelBtn = document.querySelector('#btn-cancel');
 const confirmBtn = document.querySelector('#btn-confirm');
 const expensesList = document.querySelector('#expenses-list');
 
+const clear = () => {
+    reason.value = '';
+    amount.value = '';
+};
+
 confirmBtn.addEventListener('click', () => {
     const newItem = document.createElement('ion-item');
     newItem.textContent = reason.value + ': $' + amount.value;
 
     expensesList.appendChild(newItem);
+    clear();
 });
+cancelBtn.addEventListener('click', clear);
