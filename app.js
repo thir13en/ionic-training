30972477c8b176a9ad194b4fbe59e93ad12e5d4b
santiloopz <santilpzate@gmail.com>
2020-01-18 17:46:49 +0100

    achieved dynamical addition of items

--- app.js
+++ app.js
@@ -2,5 +2,11 @@ const reason = document.querySelector('#reason');
 const amount = document.querySelector('#amount');
 const cancelBtn = document.querySelector('#btn-cancel');
 const confirmBtn = document.querySelector('#btn-confirm');
+const expensesList = document.querySelector('#expenses-list');
 
-confirmBtn.addEventListener('click', () => console.log('wired!'));
+confirmBtn.addEventListener('click', () => {
+    const newItem = document.createElement('ion-item');
+    newItem.textContent = reason.value + ': $' + amount.value;
+
+    expensesList.appendChild(newItem);
+});
