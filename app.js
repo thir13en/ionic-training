91aeb3591b140a3c2732742d4cd21735843dc0a5
santiloopz <santilpzate@gmail.com>
2020-01-19 18:46:05 +0100

    added alert and input validation

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cancelBtn = document.querySelector('#btn-cancel');
 const confirmBtn = document.querySelector('#btn-confirm');
 const expensesList = document.querySelector('#expenses-list');
 const expensesOutput = document.querySelector('#total-expenses');
+const alertCtrl = document.querySelector('ion-alert-controller');
 
 let totalExpenses = 0;
 
@@ -16,9 +17,24 @@ confirmBtn.addEventListener('click', () => {
     const newItem = document.createElement('ion-item');
     newItem.textContent = reason.value + ': $' + amount.value;
 
-    expensesList.appendChild(newItem);
-    totalExpenses += +amount.value;
-    expensesOutput.textContent = totalExpenses;
-    clear();
+    if (
+        reason.value.trim().length <= 0 ||
+        amount.value <= 0 ||
+        amount.value.trim().length <= 0
+    ) {
+        console.log('wired');
+        alertCtrl.create({
+            header: 'Invalid inputs',
+            message: 'Please enter a valir reason and amount',
+            buttons: ['OK']
+        }).then(
+            alertEl => alertEl.present()
+        );
+    } else {
+        expensesList.appendChild(newItem);
+        totalExpenses += +amount.value;
+        expensesOutput.textContent = totalExpenses;
+        clear();
+    }
 });
 cancelBtn.addEventListener('click', clear);
