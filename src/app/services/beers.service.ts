a1b7ad5bcc2d52509b39465f2e04c8d412697f66
santiloopz <santilpzate@gmail.com>
2020-01-25 08:04:35 +0100

    created beers service to serve beer

---
+++ src/app/services/beers.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {Beer} from '../core/interfaces/beer';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BeersService {
+  private beers: Beer[] = [
+    {
+      id: '1',
+      name: 'Mumbai',
+      imageUrl: 'https://media.area-gourmet.com/c/product/cerveza-vic-brewery-mumbai-520x520.jpeg',
+      artisan: true
+    },
+    {
+      id: '1',
+      name: 'Montseny',
+      imageUrl: 'https://cdn.shopify.com/s/files/1/0137/9872/products/MONTSENY-IPA-_ampolla_grande.jpg?v=1422615773',
+      artisan: true
+    }
+  ];
+
+  getBeers(): Beer[] {
+    return [...this.beers];
+  }
+
+  getBeer(beerId: string): Beer {
+    return { ...this.beers.find((beer: Beer) => beer.id === beerId) };
+  }
+
+}
