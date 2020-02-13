ab7189c9a97666d9e9c0393452ff429966a358b0
santiloopz <santilpzate@gmail.com>
2020-02-13 12:46:39 +0100

    added auth page and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,10 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'beers', pathMatch: 'full' },
   {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthPageModule)
+  },
+  {
     path: 'beers',
     loadChildren: () => import('./beers/beers.module').then( m => m.BeersPageModule)
   },
