7d73ad15042efe5d245264e187d24d0c72a9a452
santiloopz <santiago.lopez@i2cat.net>
2020-02-18 12:34:02 +0100

    moved beer item

--- src/app/beers/discover/discover.module.ts
+++ src/app/beers/discover/discover.module.ts
@@ -7,7 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { DiscoverPageRoutingModule } from './discover-routing.module';
 
 import { DiscoverPage } from './discover.page';
-import { BeerItemComponent } from '@app/beers/beer-item/beer-item.component';
+import { BeerItemComponent } from '@app/beers/discover/beer-item/beer-item.component';
 
 
 @NgModule({
