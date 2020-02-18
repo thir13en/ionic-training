dc6ff4bd64f9a662126de09f5c9ff601ae4990d7
santiloopz <santiago.lopez@i2cat.net>
2020-02-18 12:20:52 +0100

    updated app routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,10 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
 })
 export class AppComponent {
   constructor(
