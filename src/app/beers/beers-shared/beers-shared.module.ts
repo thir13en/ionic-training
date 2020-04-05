15003985046b674453419d11bceb9bda6ac8a14a
thir13en <santilpzate@gmail.com>
2020-04-05 20:17:18 +0200

    added TODO for router design error

--- src/app/beers/beers-shared/beers-shared.module.ts
+++ src/app/beers/beers-shared/beers-shared.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 
 import { BeerItemComponent } from './components/beer-item/beer-item.component';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
+  imports: [
+      // TODO: facing design error when it comes to routing
+      RouterModule,
+      IonicModule,
+  ],
   declarations: [BeerItemComponent],
   exports: [BeerItemComponent],
 })
