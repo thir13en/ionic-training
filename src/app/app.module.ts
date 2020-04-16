5222ff4279cb64ce7e40ead3fbce5a9a442c8231
thir13en <santilpzate@gmail.com>
2020-04-16 16:56:36 +0200

    added routes to menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,10 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { SidenavMenuComponent } from './layout/components';
 
 
 @NgModule({
-  declarations: [
-      AppComponent,
-      SidenavMenuComponent,
-  ],
+  declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     StatusBar,
