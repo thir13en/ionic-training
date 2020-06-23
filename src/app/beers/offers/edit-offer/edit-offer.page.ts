aab00c3c566276ea9e68f5a3e3cc792ea7558481
thir13en <santilpzate@gmail.com>
2020-06-23 10:38:32 +0200

    configuring the edit offer page

--- src/app/beers/offers/edit-offer/edit-offer.page.ts
+++ src/app/beers/offers/edit-offer/edit-offer.page.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { Observable } from 'rxjs';
+
+import { Beer } from '@core/interfaces';
+import { BeersService } from '@app/services';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs/operators';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-edit-offer',
   templateUrl: './edit-offer.page.html',
   styleUrls: ['./edit-offer.page.scss'],
 })
-export class EditOfferPage implements OnInit {
+export class EditOfferPage {
 
-  constructor() { }
+  beers$: Observable<Beer[]>;
+  beer$: Observable<Beer>;
 
-  ngOnInit() {
+  constructor(
+      private activatedRoute: ActivatedRoute,
+      private navCtrl: NavController,
+      private beersService: BeersService
+  ) { }
+
+  ionViewWillEnter() {
+    this.beers$ = this.beersService.getOffers$();
+
+    this.activatedRoute.paramMap.subscribe(paramMap => {
+      const beerId = paramMap.get('beerId');
+      if (beerId) {
+        this.beer$ = this.beersService.getOffers$().pipe(
+            map(beers => beers.filter(beer => beer.id === beerId)[0]),
+        );
+      } else {
+        this.navCtrl.navigateBack('/beers/tabs/offers/');
+      }
+    });
   }
 
 }
