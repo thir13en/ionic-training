28a7d1de1789b2df9891f0c54d4eada567d6d41a
santiloopz <santilpzate@gmail.com>
2020-08-10 09:52:34 +0200

    fixing test suite

--- src/app/beers/brews/services/brews.service.spec.ts
+++ src/app/beers/brews/services/brews.service.spec.ts
@@ -3,10 +3,12 @@ import { TestBed } from '@angular/core/testing';
 import { BrewsService } from './brews.service';
 
 describe('BrewsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [BrewsService],
+  }));
 
   it('should be created', () => {
-    const service: BrewsService = TestBed.get(BrewsService);
+    const service: BrewsService = TestBed.inject(BrewsService);
     expect(service).toBeTruthy();
   });
 });
