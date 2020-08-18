7fc6d41d01e9ef22455c4b91448749df0d4b817b
santiloopz <santilpzate@gmail.com>
2020-08-18 09:55:12 +0200

    working on eddit offer page tests

--- src/app/beers/offers/edit-offer/edit-offer.page.spec.ts
+++ src/app/beers/offers/edit-offer/edit-offer.page.spec.ts
@@ -1,11 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
-import { EMPTY } from 'rxjs';
+import { EMPTY, of } from 'rxjs';
 
 import { TestingModule } from '@testing/testing.module';
 import { BeersService } from '@app/services';
 import { EditOfferPage } from './edit-offer.page';
+import { ActivatedRoute } from '@angular/router';
 
 
 fdescribe('EditOfferPage', () => {
@@ -20,14 +21,15 @@ fdescribe('EditOfferPage', () => {
         IonicModule.forRoot(),
       ],
       declarations: [EditOfferPage],
+      providers: [{ provide: ActivatedRoute, useValue: { paramMap: of({ get: () => undefined }) } }],
     }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(EditOfferPage);
     component = fixture.componentInstance;
-    beersService = TestBed.inject(BeersService);
-    spyOn(beersService, 'getBeers$').and.returnValue(EMPTY);
+    // beersService = TestBed.inject(BeersService);
+    // spyOn(beersService, 'getBeers$').and.returnValue(EMPTY);
     fixture.detectChanges();
   });
 
