d01c26025802ace450497a8f9bd42891a29dbd37
santiloopz <santilpzate@gmail.com>
2020-08-22 22:29:17 +0200

    installed husky and faker and fixed more test

--- src/app/testing/testing.module.ts
+++ src/app/testing/testing.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { NoopComponent } from '@testing/mocks';
-import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
     exports: [
         CommonModule,
         FormsModule,
+        ReactiveFormsModule,
         RouterTestingModule,
         HttpClientTestingModule,
     ],
